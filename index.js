3a75fd226fc6adc72654bd3ada1e032c30e62396
WeiyeeGoh <WeiyeeGoh@users.noreply.github.com>
2019-06-10 22:49:25 -0700

    adding new shit

--- index.js
+++ index.js
@@ -40,76 +40,123 @@ app.get('/watch/:genLink', function (req, res) {
   console.log("GENLINK: " + req.params["genLink"]);
   res.render('index', {token: req.params["genLink"]});
 });
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
 http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  var room_name = socket.handshake.query.token;                               //Our room
-  var cur_room_color = room_info[room_name]["colors_left"].pop(0);          //Increments our room count
-  var socket_name = socket;
-  room_info[room_name]["user_list"].push(socket);                           //add socket to our user
-  socket.join(room_name);                                                   //Join that room
-  room_info[room_name]["room_count"] += 1;
-  console.log("Room Count: " + room_info[room_name]["room_count"]);          //Print out some values 
-  console.log("Room Color: " + cur_room_color);
-  console.log("param: " + socket.handshake.query.token)
-  console.log('user ' + socket.id + ' connected');
 
-  socket.on('pause', function(){
-    socket.broadcast.emit('pause');
-  });
-  socket.on('play', function(){
-    socket.broadcast.emit('play');
-  });
-  socket.on('seek', function(param){
-    socket.broadcast.emit('seek', param);
-  });
+  console.log(socket.handshake.query.type);
+  console.log(socket.handshake.query.token);
 
-  socket.on('activity log', function(msg){
-    var msgObject = {
-      "message": msg, 
-      "color": cur_room_color
-    }
-    io.in(Object.keys(socket.rooms)[1]).emit('activity log', JSON.stringify(msgObject));
+  if (socket.handshake.query.type == "watch") {
+    console.log('a user connected');
+    var room_name = socket.handshake.query.token;                               //Our room
+    var cur_room_color = room_info[room_name]["colors_left"].pop(0);          //Increments our room count
+    var socket_name = socket;
+    room_info[room_name]["user_list"].push(socket);                           //add socket to our user
+    socket.join(room_name);                                                   //Join that room
+    room_info[room_name]["room_count"] += 1;
+    console.log("Room Count: " + room_info[room_name]["room_count"]);          //Print out some values 
+    console.log("Room Color: " + cur_room_color);
+    console.log("param: " + socket.handshake.query.token)
+    console.log('user ' + socket.id + ' connected');
 
-  });
+    socket.on('pause', function(){
+      socket.in(room_name).emit('pause');
+    });
+    socket.on('play', function(){
+      socket.in(room_name).emit('play');
+    });
+    socket.on('seek', function(param){
+      socket.in(room_name).emit('seek', param);
+    });
 
-  socket.on('chat message', function(msg){
-    //Our message is a json object. We will parse it on client side. 
-    msg = JSON.parse(msg);
-    var msgObject = {
-      "name": msg["name"],
-      "message": msg["message"], 
-      "color": cur_room_color
-    }
-    console.log("COLOR: " + msgObject["color"]);
+    socket.on('activity log', function(msg){
+      var msgObject = {
+        "message": msg, 
+        "color": cur_room_color
+      }
+      io.in(room_name).emit('activity log', JSON.stringify(msgObject));
+    });
 
-    //converting our object to string
-    var msg_s = JSON.stringify(msgObject);
+    socket.on('chat message', function(msg){
+      //Our message is a json object. We will parse it on client side. 
+      msg = JSON.parse(msg);
+      var msgObject = {
+        "name": msg["name"],
+        "message": msg["message"], 
+        "color": cur_room_color
+      }
+      console.log("COLOR: " + msgObject["color"]);
 
-    //Sends a message to all clients inside our room
-    io.in(Object.keys(socket.rooms)[1]).emit('chat message', msg_s);
-    //Print out socket id, msg
-    console.log('message from socket ' + socket.id + ': ' + msg);
-    console.log('Room: ' + room_name);
-    console.log(Object.keys(socket.rooms)); //Will get an array of the key + all the rooms. Ill just leave this here cz it might be useful later 
-  });
+      //converting our object to string
+      var msg_s = JSON.stringify(msgObject);
 
-  //Client has disconnected from our server
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-    //We will decrement the number of users in our room
-    room_info[room_name]["colors_left"].push(cur_room_color);
-    room_info[room_name]["room_count"] -= 1;
+      //Sends a message to all clients inside our room
+      io.in(room_name).emit('chat message', msg_s);
+      //Print out socket id, msg
+      console.log('message from socket ' + socket.id + ': ' + msg_s);
+      console.log('Room: ' + room_name);
+      console.log("ROOOOOMS: " + socket.rooms);
+    });
 
-    var index = room_info[room_name]["user_list"].indexOf(socket_name);
-    if (index > -1) {
-      room_info[room_name]["user_list"].splice(index, 1);
-    }
-    for(var i = 0; i < room_info[room_name]["user_list"].length; i++) {
-      console.log("array after removal: " + room_info[room_name]["user_list"][i].id);
-    }
+    //Client has disconnected from our server
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+      //We will decrement the number of users in our room
+      room_info[room_name]["colors_left"].push(cur_room_color);
+      room_info[room_name]["room_count"] -= 1;
 
-  });
+      var index = room_info[room_name]["user_list"].indexOf(socket_name);
+      if (index > -1) {
+        room_info[room_name]["user_list"].splice(index, 1);
+      }
+      for(var i = 0; i < room_info[room_name]["user_list"].length; i++) {
+        console.log("array after removal: " + room_info[room_name]["user_list"][i].id);
+      }
+
+    });
+  } else if (socket.handshake.query.type == "home") {
+
+    socket.on("create_room", function(param) {
+
+      var room_token = Math.floor(Math.random() * 1000);
+      while(room_token in room_info) {
+        room_token = Math.floor(Math.random() * 1000);
+      }
+
+      room_info[room_token.toString()] = {
+        "colors_left": [],
+        "room_count": 0,
+        "user_list": []
+      };
+
+      console.log(room_token);
+      console.log("rooms");
+      for(var key in room_info) {
+        console.log(key);
+      };
+
+      for(var i = 0; i < colors.length; i++) {
+        room_info[room_token]["colors_left"].push(colors[i]);
+      }
+
+      var linkObject = {
+        "port": PORT, 
+        "route": "/watch",
+        "token": room_token
+      };
+       var msg_s = JSON.stringify(linkObject);
+        socket.emit('create_room', msg_s)
+    });
+
+    socket.on('disconnect', function(){
+      console.log('home page user disconnected');
+    });
+  }
 });
 
